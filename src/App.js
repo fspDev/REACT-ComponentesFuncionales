43be629f93b08343c826af5645625c6565170fff
Franco Pereyra <di.francopereyra@gmail.com>
2023-06-04 11:11:23 -0300

    proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import Container from '@mui/material/Container';
 import FormSignUp from './Components/FormSignUp';
+import Typography from '@mui/material/Typography';
 
 function App() {
+
+  const handleSubmit = (valores) => {
+    console.log("APPJS: ",valores)
+  }
+
   return (
-    <>
-      <h1>Formulario de Registro</h1>
-      <FormSignUp/>
-    </>
+    <Container component="section" maxWidth="sm">
+      <Typography variant="h3" align="center">Formulario de Registro</Typography>
+      <FormSignUp handleSubmit={handleSubmit}/>
+    </Container>
   );
 }
 
